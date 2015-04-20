5c945f7fef4c58efbefffe081eafe0fae7b94fa7
Nick Hoobin <nick@webspecdesign.com>
2015-04-20 10:36:37 -0500

    Remove apple touch icon

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" href="<?php echo THEMEROOT; ?>/favicon.png">
 <!--[if IE]><link rel="shortcut icon" href="<?php echo THEMEROOT; ?>favicon.ico"/><![endif]-->
-<link rel="apple-touch-icon" href="apple-touch-icon.png">
 <?php wp_head(); ?>
 </head>
 <body>
