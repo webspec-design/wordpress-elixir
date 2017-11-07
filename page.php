0d4450396b4af555dc6f2d61c250c9974784e012
Nick Hoobin <nick@webspecdesign.com>
2017-11-07 14:25:38 -0600

    Add 404

--- page.php
+++ page.php
@@ -4,8 +4,13 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-md-8 col-md-offset-2">
-				<h1><?php the_title(); ?></h1>
-				<?php the_content(); ?>
+				<?php if(is_404()) : ?>
+					<h1>404: Page Not Found</h1>
+					<p>We could not find the page you were looking for. Perhaps you should return to the <a href="<?php echo home_url(); ?>">home page</a>?</p>
+				<?php else : ?>
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
